71eeff123f16a5b7704086b9d3955b990256fb2f
PatrickAdelberger <patrick.adelberger@jku.at>
2022-08-02 14:09:50 +0200

    add history functionality to table

--- src/Investigation.ts
+++ src/Investigation.ts
@@ -1,4 +1,5 @@
 import ColumnTable from "arquero/dist/types/table/column-table";
+import { Scatterplot } from "./Scatterplot";
 import { getColumnTypesFromArqueroTable } from "./util";
 import { VisType, VisualizationBase } from "./visualizations";
 
@@ -22,25 +23,32 @@ export class Investigation {
 
 
   private _dataColumns: {label: string, type: string}[]
+  dataset: ColumnTable;
 
   constructor(cntrMain: HTMLDivElement, isFreeMode: boolean, dataset: ColumnTable) {
     
     // this.$actObTable = this.createTable();
     this.$container = cntrMain;
 
+    this._visHistory = [];
+    this.dataset = dataset;
     this._dataColumns = getColumnTypesFromArqueroTable(dataset)
     console.log('data columns: ', this._dataColumns);
+
+    // setup all elemtens and their containers:
+    // current Vis, next Vis, table, previews
     this.setupInterface();
-    // create the encoding actions
+    // update the encodings in the table
     this.updateEncodings();
+    this.updateVisualizations();
 
 
     this.addEventListeners();
 
     // test columns for history
-    this.addHistoryColumn();
-    this.addHistoryColumn();
-    this.addHistoryColumn();
+    // this.addHistoryColumn();
+    // this.addHistoryColumn();
+    // this.addHistoryColumn();
   }
 
   setupInterface() {
@@ -80,47 +88,47 @@ export class Investigation {
     this.$container.appendChild(this.$wrapperPreview);
   }
 
-  createTable(): HTMLTableElement {
+  createTable(starVisType: VisType = VisType.Scatter): HTMLTableElement {
     const $table = document.createElement('table');
     $table.innerHTML = `
       <thead>
         <tr>
-          <th id="act-ob-table-th-history" scope="col" colspan="1">History</th>
           <th scope="col">Actions & Objectives</th>
           <th scope="col">Current</th>
         </tr>
       </thead>
       <tbody class="table-actions">
         <tr id="act-vis-type">
-          <td>h1</td>
           <td>Visualization Type</td>
           <td id="vis-type-cell"></td>
         </tr>
-        <tr>
-          <td>h1</td>
-          <td>Set X-Axis</td>
-          <td>...</td>
-        </tr>
       </tbody>
       <tbody class="table-objectives">
-        <tr>
-          <td>h1</td>
-          <td>Reduce Overplotting</td>
-          <td>OK</td>
-        </tr>
       </tbody>`; 
 
+    // <th id="act-ob-table-th-history" scope="col" colspan="1">History</th>
+    // <td>h1</td>
+    // <tr>
+    //   <td>h1</td>
+    //   <td>Set X-Axis</td>
+    //   <td>...</td>
+    // </tr>
+    // <tr>
+    //   <td>h1</td>
+    //   <td>Reduce Overplotting</td>
+    //   <td>OK</td>
+    // </tr>
 
     // add vis type select with its options to table
-    const infoSel = {id: 'vis-select'};
+    const infoSel = {id: 'vis-select', classes: ['current-state']};
     const visTypeOpt = [
       {value: ''+VisType.Scatter, text: 'Scatterplot'},
       {value: ''+VisType.Line, text: 'Line Chart'},
       {value: ''+VisType.Bar, text: 'Bar Chart'}
     ];
     const $select = this._ceateHTMLSelectWithOptions(infoSel, visTypeOpt);
-    // TODO set initial visualization type dynamically
-    $select.value = ''+VisType.Scatter
+    // set initial visualization type dynamically
+    $select.value = ''+starVisType;
 
     // add select to table cell
     const $cellVisType = $table.querySelector('#vis-type-cell');
@@ -134,59 +142,157 @@ export class Investigation {
     // vis selection
     const selectVis: HTMLSelectElement = this.$actObTable.querySelector('#vis-select');
     selectVis.addEventListener('change', (ev) => {
+      this.addHistoryColumn();
       this.updateEncodings();
-    })
-  }
-
-  updateEncodings() {
-    const selectVis: HTMLSelectElement = this.$actObTable.querySelector('#vis-select');
-    const visType = Number(selectVis.value);
-
-    const xSel = {id: 'x-axis-select'};
-    const opts = this._dataColumns.map((elem) => { 
-      return {value: elem.label.toLowerCase(), text: elem.label}
+      this.updateVisualizations();
     });
-    const $xSelect = this._ceateHTMLSelectWithOptions(xSel, opts);
 
-    const ySel = {id: 'y-axis-select'};
-    const $ySelect = this._ceateHTMLSelectWithOptions(ySel, opts);
-
-    const colorSel = {id: 'color-select'};
-    const $colorSelect = this._ceateHTMLSelectWithOptions(colorSel, opts);
-
-
-    if(visType === VisType.Scatter) {
-      console.log('new value: ', selectVis.value);
-      this.addRow(2,'Set X-Axis Encoding', $xSelect);
-      this.addRow(3,'Set Y-Axis Encoding', $ySelect);
-      this.addRow(4,'Set Color Encoding', $colorSelect);
-
-    } else if (visType === VisType.Line) {
-      console.log('new value: ', selectVis.value);
-      this.addRow(2,'Set X-Axis Encoding', $xSelect);
-      this.addRow(3,'Set Y-Axis Encoding', $ySelect);
-      this.addRow(4,'Set Color Encoding', $colorSelect);
-    } else {
-      console.log('new value: ', selectVis.value);
-      this.addRow(2,'Set X-Axis Encoding', $xSelect);
-      this.addRow(3,'Set Y-Axis Encoding', $ySelect);
+    // encoding selection
+    const selectEncs: HTMLSelectElement[] = Array.from(this.$actObTable.querySelectorAll('.encoding-selecion'));
+    for(const sel of selectEncs) {
+      sel.addEventListener('change', (ev) => {
+        this.addHistoryColumn();
+        this.updateVisualizations();
+      });
     }
   }
 
-  addRow(posFromTop: number, label: string, currElem: HTMLElement) {
+  updateEncodings() {
+    console.log('update Encoding');
+    const selectVis: HTMLSelectElement = this.$actObTable.querySelector('#vis-select');
+    const visType = Number(selectVis.value);
+
+    this.updateEncodingRows(visType);
+  }
+
+  updateEncodingRows(visType: VisType) {
+    //get current attibure of dataset
+    const opts = this._dataColumns.map((elem) => { 
+      return {value: elem.label, text: elem.label}
+    });
+
+    // add empty option
+    opts.unshift({value: 'null', text: ''});
+
+    const xSel = {id: 'x-axis-select', classes: ['encoding-selecion', 'current-state']};
+    const $xSelect = this._ceateHTMLSelectWithOptions(xSel, opts);
+
+    const ySel = {id: 'y-axis-select', classes: ['encoding-selecion', 'current-state']};
+    const $ySelect = this._ceateHTMLSelectWithOptions(ySel, opts);
+
+    const colorSel = {id: 'color-select', classes: ['encoding-selecion', 'current-state']};
+    const $colorSelect = this._ceateHTMLSelectWithOptions(colorSel, opts);
+
+    // check if encoding rows exist
+    const xEncRow: HTMLTableRowElement = this.$actObTable.querySelector('#x-axis-encoding');
+    const yEncRow: HTMLTableRowElement = this.$actObTable.querySelector('#y-axis-encoding');
+    const cEncRow: HTMLTableRowElement = this.$actObTable.querySelector('#color-encoding');
+
+    
+    // for all charts x and y encodings are used
+    if(!xEncRow) {
+      // console.log('xEncRow: ',xEncRow);
+      // table data cell
+      const elemTd = document.createElement('td');
+      elemTd.appendChild($xSelect);
+      this.addRow(2,'x-axis-encoding','Set X-Axis Encoding', elemTd);
+    }
+
+    if(!yEncRow) {
+      // console.log('yEncRow: ',yEncRow);
+      // table data cell
+      const elemTd = document.createElement('td');
+      elemTd.appendChild($ySelect);
+      this.addRow(3,'y-axis-encoding','Set Y-Axis Encoding', elemTd);
+    }
+
+    
+    if(visType === VisType.Scatter || visType === VisType.Line) {
+      // for Scatterplot and Line chart, all encodings are used (x,y,color)
+      // add color endoding
+      if(!cEncRow) {
+        // console.log('cEncRow: ',cEncRow);
+        // table data cell
+        const elemTd = document.createElement('td');
+        elemTd.appendChild($colorSelect);
+        this.addRow(4,'color-encoding','Set Color Encoding', elemTd);
+      }
+  
+      // this.addRow(2,'x-axis-encoding','Set X-Axis Encoding', $xSelect);
+      // this.addRow(3,'y-axis-encoding','Set Y-Axis Encoding', $ySelect);
+      // this.addRow(4,'color-encoding','Set Color Encoding', $colorSelect);
+
+    } else if (visType === VisType.Bar) {
+      // for Bar chart only x and y encodings are used
+      // make sure color encoding is removed
+      if(cEncRow) {
+        // console.log('cEncRow: ',xEncRow);
+        this.removeRow('color-encoding');
+      }
+      // this.addRow(2,'x-axis-encoding','Set X-Axis Encoding', $xSelect);
+      // this.addRow(3,'y-axis-encoding','Set Y-Axis Encoding', $ySelect);
+      // this.addRow(4,'color-encoding','Set Color Encoding', $colorSelect);
+    } 
+    //else {
+    //   this.addRow(2,'x-axis-encoding','Set X-Axis Encoding', $xSelect);
+    //   this.addRow(3,'y-axis-encoding','Set Y-Axis Encoding', $ySelect);
+    // }
+  }
+
+  async updateVisualizations() {
+    console.log('update Visualization');
+    const selectVis: HTMLSelectElement = this.$actObTable.querySelector('#vis-select');
+    const selectX: HTMLSelectElement = this.$actObTable.querySelector('#x-axis-select');
+    const selectY: HTMLSelectElement = this.$actObTable.querySelector('#y-axis-select');
+    const selectC: HTMLSelectElement = this.$actObTable.querySelector('#color-select');
+ 
+    // console.log('dataset Investigation: ', this.dataset);
+    const scatter = new Scatterplot('test', this.dataset, selectX.value, selectY.value, selectC.value);
+
+    const newStep = this._visHistory.length+1;
+    this._visHistory.push({step: newStep, score: 0, visualization: scatter});
+
+    // clear current Vis container
+    this.$currVis.textContent = '';
+
+    // create container for new visualization
+    const visItem = document.createElement('div');
+    visItem.classList.add('vis-item');
+    // add vis container to vis strip
+    this.$currVis.appendChild(visItem);
+
+    console.log('visHistory: ', this._visHistory);
+    await scatter.showVisualization(visItem);
+  }
+
+  updateActions() {
+
+  }
+
+  removeRow(rowId: string) {
+    const rowElement: HTMLTableRowElement = this.$actObTable.querySelector(`#${rowId}`);
+
+    if(rowElement) {
+      rowElement.remove();
+    }
+
+
+  }
+  addRow(posFromTop: number, rowId: string, label: string, currElem: HTMLElement) {
     const tableHeadCellHist: HTMLTableCellElement = this.$actObTable.querySelector('#act-ob-table-th-history');
-    const currColSpan = tableHeadCellHist.colSpan;
+    const currColSpan = tableHeadCellHist ? tableHeadCellHist.colSpan : 0;
     console.log('add row: ', {label, currColSpan});
 
     const bodyAction = this.$actObTable.querySelector('tbody.table-actions');
 
     // new row that will be added to the table
     const newRow = document.createElement('tr');
+    newRow.id = rowId;
 
     // add all history states
     for(let i=0; i< currColSpan; i++) {
       const cell = document.createElement('td');
-      cell.innerHTML = '-';
+      cell.innerHTML = '';
       newRow.appendChild(cell);
     }
 
@@ -212,23 +318,67 @@ export class Investigation {
 
 
   addHistoryColumn() {
+    console.log('add History Column: ', this._visHistory);
+    // find the step number
+    const allSteps = this._visHistory.map((elem) => elem.step);
+    const numbSteps = allSteps.length;
+    const currStep = numbSteps > 0 ? allSteps[numbSteps-1] : 1;
+
+    // check if history columns already exist
+    const currTableHeadHist = this.$actObTable.querySelector('#act-ob-table-th-history');
+
+    if(!currTableHeadHist) {
+      const tableHead =  this.$actObTable.querySelector('thead');
+      const tableHeadRow = tableHead.querySelector('tr');
+      const tableHeadCellHist = document.createElement('th');
+      tableHeadCellHist.id = 'act-ob-table-th-history';
+      tableHeadCellHist.scope = 'col';
+      tableHeadCellHist.colSpan = 0;
+      tableHeadCellHist.innerHTML = 'History';
+      tableHeadRow.insertBefore(tableHeadCellHist, tableHeadRow.children[0]);
+
+      // <thead>
+      //   <tr>
+      //     <th id="act-ob-table-th-history" scope="col" colspan="1">History</th>
+    }
+
     // add column and increase colspan of the history col
+    // select all rows in table
     const rows = Array.from(this.$actObTable.querySelectorAll('tr'));
+    // go throu all the rows
     for (const row of rows) {
       // use tagName to check if row is in <thead>, https://developer.mozilla.org/en-US/docs/Web/API/Element/tagName
       if(row.parentElement.tagName === "THEAD") {
         // row in table head
         const tableHeadCellHist: HTMLTableCellElement = row.querySelector('#act-ob-table-th-history');
-        const currColSpan = tableHeadCellHist.colSpan;
-        tableHeadCellHist.colSpan = currColSpan + 1;
-        console.log('add history column: ', {oldHistStates: currColSpan, newHistStates: currColSpan+1});
+        // const currColSpan = tableHeadCellHist.colSpan;
+        // tableHeadCellHist.colSpan = currColSpan + 1;
+        // console.log('add history column: ', {oldHistStates: currColSpan, newHistStates: currColSpan+1});
+        tableHeadCellHist.colSpan = numbSteps;
+        // console.log('add history column: ', {oldHistStates: numbSteps-1, newHistStates: numbSteps});
 
       } else {
         // row in table body
+        
         // TODO add the right column values, style, ....
+        // get the current state value
+        const elemCurrState = row.querySelector('.current-state');
+        console.log('add Hist Col: current State: ', elemCurrState);
+        let currStateVal = '';
+        if(elemCurrState?.tagName === "SELECT") {
+          // vistype and encodings
+          const select = elemCurrState as HTMLSelectElement;
+          currStateVal = select.options[select.selectedIndex].text;
+        } else {
+          // actions and objectives
+          currStateVal = ''+currStep;
+        }
+
         // TODO add step variable (history steps) to column
         const cell = document.createElement('td');
-        cell.innerHTML = 'test';
+        cell.classList.add(`${currStep}`,'history-cell');
+        cell.innerHTML = currStateVal;
+        cell.title = currStateVal;
         const numbChildren = row.children.length;
         const numbColBeforeHist = 2;
         row.children[numbChildren-numbColBeforeHist].insertAdjacentElement('beforebegin', cell);

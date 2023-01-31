58c5e043e60995ec91d9238ac8321a68475e26c7
Patrick Adelberger <patrick.adelberger@jku.at>
2023-01-31 13:36:27 +0100

    update REST save function options

--- src/REST.ts
+++ src/REST.ts
@@ -86,16 +86,16 @@ export async function postJSONUserData(filename: string, data: IUserTrackData) {
     data
   };
   // console.log("🚀 ~ file: REST.ts ~ line 54 ~ postJSONGameData ~ payload", payload)
-  console.log('window.lcoation: ', window.location);
+  // console.log('window.lcoation: ', window.location);
   if(isSurvey) {
     const response = await fetch(postURL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         // "Access-Control-Allow-Origin": "http://localhost"
-        "Access-Control-Allow-Origin": "https://jku-vds-lab.at",
-        "Access-Control-Request-Method": "POST",
-        "Access-Control-Request-Headers": "Content-Type"
+        // "Access-Control-Allow-Origin": "https://jku-vds-lab.at",
+        // "Access-Control-Request-Method": "POST",
+        // "Access-Control-Request-Headers": "Content-Type"
       },
       body: JSON.stringify(payload)
     });
